3bd8c71b36b2c7144119ad66f3ce163cdd7954f0
Amtavi-Brian <brian.amtavi@strathmore.edu>
2026-08-29 17:50:02 +0300

    database

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+from database import engine, Base
+import models
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-todos = [
-    {"id": 1, "title": "Learn FastAPI", "completed": False},
-    {"id": 2, "title": "Build a Todo API", "completed": False},
-]
 
-
-class Todo(BaseModel):
-    title: str
-    completed: bool = False
-
-
-@app.get("/todos")
-def get_todos():
-    return todos
-
-
-@app.post("/todos")
-def create_todo(todo: Todo):
-    new_todo = {
-        "id": len(todos) + 1,
-        "title": todo.title,
-        "completed": todo.completed
-    }
-
-    todos.append(new_todo)
-
-    return new_todo
+@app.get("/")
+def home():
+    return {"message": "Todo API is running"}
