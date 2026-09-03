33eaa2a7141eda6dff24c0340ac2da4a0036b3c5
Amtavi-Brian <brian.amtavi@strathmore.edu>
2026-09-03 18:32:05 +0300

    Users Table

--- app/models.py
+++ app/models.py
@@ -1,6 +1,25 @@
+from operator import index
+
 from sqlalchemy import Column, DateTime, Integer, String, Boolean
 from app.database import Base
 from datetime import datetime
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False, index=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    password_hash = Column(String, nullable=False)
+
+    created_at = Column(
+        DateTime,
+        default=datetime.utcnow,
+        nullable=False,
+    )
+
+
 
 class Todo(Base):
     __tablename__ = "todos"
